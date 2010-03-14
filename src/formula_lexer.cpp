f48e32f5eef8faed562d28a11b81f3c34a80aebd
Kohei Yoshida <kyoshida@novell.com>
2010-03-14 14:04:10 -0400

    Main loop for tokenizer.

--- src/formula_lexer.cpp
+++ src/formula_lexer.cpp
@@ -27,9 +27,116 @@
 
 #include "formula_lexer.hpp"
 
+#include <iostream>
+
 using namespace std;
 
 namespace ixion {
+
+// ============================================================================
+
+class tokenizer : public ::boost::noncopyable
+{
+public:
+    tokenizer(tokens_t& tokens, string& formula) :
+        m_tokens(tokens), m_formula(formula), m_pos(0), m_size(formula.size()) {}
+
+    void run();
+
+private:
+    void numeral();
+    void name();
+    void plus();
+    void minus();
+    void divide();
+    void multiply();
+    void open_bracket();
+    void close_bracket();
+
+private:
+    tokens_t& m_tokens;
+    string& m_formula;
+    size_t m_pos;
+    size_t m_size;
+};
+
+void tokenizer::run()
+{
+    for (m_pos = 0; m_pos < m_size; ++m_pos)
+    {
+        char c = m_formula[m_pos];
+        cout << "char: '" << c << "'" << endl;
+        switch (c)
+        {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                numeral();
+                break;
+            case '+':
+                plus();
+                break;
+            case '-':
+                minus();
+                break;
+            case '/':
+                divide();
+                break;
+            case '*':
+                multiply();
+                break;
+            case '(':
+                open_bracket();
+                break;
+            case ')':
+                close_bracket();
+                break;
+            default:
+                name();
+        }
+    }
+}
+
+void tokenizer::numeral()
+{
+}
+
+void tokenizer::name()
+{
+}
+
+void tokenizer::plus()
+{
+}
+
+void tokenizer::minus()
+{
+}
+
+void tokenizer::divide()
+{
+}
+
+void tokenizer::multiply()
+{
+}
+
+void tokenizer::open_bracket()
+{
+}
+
+void tokenizer::close_bracket()
+{
+}
+
+// ============================================================================
 
 formula_lexer::formula_lexer(const string& formula) :
     m_formula(formula)
@@ -42,7 +149,9 @@ formula_lexer::~formula_lexer()
 
 void formula_lexer::tokenize()
 {
-
+    cout << "formula: " << m_formula << endl;
+    tokenizer tkr(m_tokens, m_formula);
+    tkr.run();
 }
 
 void formula_lexer::swap_tokens(tokens_t& tokens)
