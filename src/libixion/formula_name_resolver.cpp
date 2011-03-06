deddd7c3896bc0191de131e8e5d520a00761d3b3
Kohei Yoshida <kyoshida@novell.com>
2011-03-06 02:24:22 -0500

    Added new files for formula name resolver classes.

---
+++ src/libixion/formula_name_resolver.cpp
@@ -0,0 +1,43 @@
+/*************************************************************************
+ *
+ * Copyright (c) 2011 Kohei Yoshida
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ ************************************************************************/
+
+#include "formula_name_resolver.hpp"
+
+using namespace std;
+
+namespace ixion {
+
+formula_name_resolver_base::~formula_name_resolver_base() {}
+
+formula_name_resolver_a1::~formula_name_resolver_a1() {}
+
+formula_name_type formula_name_resolver_a1::resolve(const string& name) const
+{
+    return invalid;
+}
+
+}
