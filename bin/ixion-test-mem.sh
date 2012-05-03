9b1b846b37c77688b975adfec3d0c0ae96a1a60e
Kohei Yoshida <kohei.yoshida@gmail.com>
2012-05-03 17:26:07 -0400

    New scripts to enable gdb and valgrind runs during test.

---
+++ bin/ixion-test-mem.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+PROGDIR=`dirname $0`
+EXEC=$PROGDIR/../src/.libs/ixion-test
+export LD_LIBRARY_PATH=$PROGDIR/../src/libixion/.libs
+valgrind --tool=memcheck --leak-check=full $EXEC "$@"
+
