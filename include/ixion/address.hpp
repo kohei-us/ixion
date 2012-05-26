826f07c3d21e2215b15584844b8d899940f59476
Kohei Yoshida <kohei.yoshida@gmail.com>
2012-05-26 18:25:34 -0400

    Updating the years of my copyright.

--- include/ixion/address.hpp
+++ include/ixion/address.hpp
@@ -1,6 +1,6 @@
 /*************************************************************************
  *
- * Copyright (c) 2011 Kohei Yoshida
+ * Copyright (c) 2011-2012 Kohei Yoshida
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
