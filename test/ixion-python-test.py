0f459cb681509923dba1b956298c302410e5ac3b
Kohei Yoshida <kohei.yoshida@gmail.com>
2015-02-01 12:53:11 -0500

    python: add test cases for string value I/O.

--- test/ixion-python-test.py
+++ test/ixion-python-test.py
@@ -56,6 +56,27 @@ class Test(unittest.TestCase):
             check_val = sh1.get_numeric_value(column=test[1], row=test[0]) # swap row and column
             self.assertEqual(test[2], check_val)
 
+    def test_string_cell_input(self):
+        sh1 = self.doc.append_sheet("Data")
+
+        # Empty cell should yield an empty string.
+        check_val = sh1.get_string_value(0, 0)
+        self.assertEqual("", check_val)
+
+        tests = (
+            # row, column, value
+            (0, 0, "normal string"),  # normal string
+            (1, 0, "A1+B1"),          # string that looks like a formula expression
+            (2, 0, "'single quote'"), # single quote
+            (3, 0, "80's music"),     # single quote
+            (4, 0, '"The" Music in the 80\'s'), # single and double quotes mixed
+        )
+
+        for test in tests:
+            sh1.set_string_cell(test[0], test[1], test[2]) # row, column, value
+            check_val = sh1.get_string_value(column=test[1], row=test[0]) # swap row and column
+            self.assertEqual(test[2], check_val)
+
 
 if __name__ == '__main__':
     unittest.main()
