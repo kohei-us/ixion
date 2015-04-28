1f6781495eb6ec4b8dbb208c8102b2739a4ec7c2
Kohei Yoshida <kohei.yoshida@gmail.com>
2015-04-27 22:25:23 -0400

    Allow use of valgrind and gdb with unit test binaries.

--- bin/ixion-test.sh
+++ bin/ixion-test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 PROGDIR=`dirname $0`
-EXEC=$PROGDIR/../src/.libs/ixion-test
-export LD_LIBRARY_PATH=$PROGDIR/../src/libixion/.libs
-$EXEC "$@"
+source $PROGDIR/ixion-common-func.sh
+ixion_exec ixion-test "$PWD" "$@"
 
