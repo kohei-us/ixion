a2cfa39722bea16ea71475bb0fa2003b5920d7ee
Kohei Yoshida <kohei.yoshida@collabora.com>
2015-08-22 15:30:16 -0400

    Make sure that we pick up the shared object from the build first.

--- bin/run-python.sh
+++ bin/run-python.sh
@@ -4,6 +4,8 @@ PROGDIR=`dirname $0`
 IXION_PYTHONPATH="$PROGDIR/../src/python/.libs"
 
 export PYTHONPATH=$IXION_PYTHONPATH
+export LD_LIBRARY_PATH="$PROGDIR/../src/libixion/.libs"
+export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
 exec $PWD/"$1"
 
 
