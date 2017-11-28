26832e00afafbfa97c078765178e20d1ce9fcac2
Kohei Yoshida <kohei.yoshida@gmail.com>
2017-11-28 18:53:34 -0500

    Change it so that new module types can be added later.

--- src/libixion/module.cpp
+++ src/libixion/module.cpp
@@ -9,6 +9,7 @@
 #include "ixion/info.hpp"
 #include "ixion/compute_engine.hpp"
 #include <sstream>
+#include <vector>
 #include <dlfcn.h>
 
 namespace ixion {
@@ -19,24 +20,35 @@ void init_modules()
     if (!module_path)
         return;
 
-    // TODO: use boost.filesystem.
+    static std::vector<const char*> mod_names = {
+        "cuda",
+    };
+
     std::ostringstream os;
-    os << module_path << "/ixion-" << get_api_version_major() << "." << get_api_version_minor() << "-cuda.so";
-    std::string cuda_path = os.str();
+    os << "ixion-" << get_api_version_major() << "." << get_api_version_minor() << "-";
+    std::string mod_prefix = os.str();
 
-    // TODO: make this cross-platform.
-    void* hdl = dlopen(cuda_path.data(), RTLD_NOW | RTLD_GLOBAL);
-    if (!hdl)
-        return;
-
-    typedef module_def* (*register_module_type)(void);
-    register_module_type register_module;
-    *(void**)(&register_module) = dlsym(hdl, "register_module");
-
-    if (register_module)
+    for (const char* mod_name : mod_names)
     {
-        module_def* md = register_module();
-        compute_engine::add_class("cuda", md->create_compute_engine, md->destroy_compute_engine);
+        // TODO: use boost.filesystem.
+        std::ostringstream os;
+        os << module_path << "/" << mod_prefix << mod_name << ".so";
+        std::string cuda_path = os.str();
+
+        // TODO: make this cross-platform.
+        void* hdl = dlopen(cuda_path.data(), RTLD_NOW | RTLD_GLOBAL);
+        if (!hdl)
+            return;
+
+        typedef module_def* (*register_module_type)(void);
+        register_module_type register_module;
+        *(void**)(&register_module) = dlsym(hdl, "register_module");
+
+        if (register_module)
+        {
+            module_def* md = register_module();
+            compute_engine::add_class("cuda", md->create_compute_engine, md->destroy_compute_engine);
+        }
     }
 }
 
