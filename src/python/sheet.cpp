2a5d537d0f8af4f8309bd14854127a33f5099e78
Kohei Yoshida <kohei.yoshida@gmail.com>
2018-10-08 07:31:18 -0400

    We don't need to declare a formula cell a modified cell.
    A formula cel becomes a dirty cell if edited, which implies that
    it's a modified cell from the implementation's prospective.

--- src/python/sheet.cpp
+++ src/python/sheet.cpp
@@ -150,7 +150,6 @@ PyObject* sheet_set_formula_cell(sheet* self, PyObject* args, PyObject* kwargs)
     ixion::model_context& cxt = sd->m_global->m_cxt;
 
     ixion::abs_address_t pos(sd->m_sheet_index, row, col);
-    sd->m_global->m_modified_cells.insert(pos);
     sd->m_global->m_dirty_formula_cells.insert(pos);
 
     ixion::formula_tokens_t tokens =
