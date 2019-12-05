c6dbafb9be570d191fe3bfa7513c95e0688cd29f
Kohei Yoshida <kohei.yoshida@gmail.com>
2019-12-04 19:08:29 -0500

    Quick-n-dirty script to extract function names from libreoffice code.
    Use this script on formula/inc/core_resource.hrc.

---
+++ misc/libo-scan-function-names.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Use this script on formula/inc/core_resources.hrc in libreoffice repo.
+
+cat $1 \
+    | grep -E "NC_.*FUNCTION_NAMES" \
+    | sed -e "s/.*_FUNCTION_NAMES\",\ \"//g" -e "s/\").*//g" \
+    | grep -v "#" \
+    | grep -v "\." \
+    | grep -v "_OOO"
