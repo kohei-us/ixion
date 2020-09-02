f7cd800a2787c112b2e1da13249f1c30f940531a
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-09-01 20:44:59 -0400

    Add script to generate the release page template.

---
+++ bin/gen-gfm-release-table.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+PKG_PREFIX=libixion
+
+# Pick up the version number string from configure.ac.
+VER_MAJOR=$(cat ./configure.ac | grep -E "ixion_major_version.*[0-9]" | sed -e "s/.*\[\([0-9][0-9]*\).*/\1/g")
+VER_MINOR=$(cat ./configure.ac | grep -E "ixion_minor_version.*[0-9]" | sed -e "s/.*\[\([0-9][0-9]*\).*/\1/g")
+VER_MICRO=$(cat ./configure.ac | grep -E "ixion_micro_version.*[0-9]" | sed -e "s/.*\[\([0-9][0-9]*\).*/\1/g")
+VER="$VER_MAJOR.$VER_MINOR.$VER_MICRO"
+
+PKGS=$(ls $PKG_PREFIX-$VER.tar.*)
+
+echo "## Release Notes"
+echo ""
+echo "* add item"
+echo ""
+echo "## Source packages for distribution"
+echo ""
+
+echo "| URL | sha256sum | size |"
+echo "|-----|-----------|------|"
+
+for _PKG in $PKGS; do
+    _URL="[$_PKG](https://kohei.us/files/$PKG_PREFIX/src/$_PKG)"
+    _HASH=$(sha256sum $_PKG | sed -e "s/^\(.*\)$PKG_PREFIX.*/\1/g" | tr -d "[:space:]")
+    _SIZE=$(stat -c "%s" $_PKG)
+    echo "| $_URL | $_HASH | $_SIZE |"
+done
+
