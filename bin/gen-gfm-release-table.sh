220f5ea876744655c32c2e27de444f77b054f999
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-09-01 20:47:34 -0400

    Use the prefix without the 'lib' for package URLs.

--- bin/gen-gfm-release-table.sh
+++ bin/gen-gfm-release-table.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-PKG_PREFIX=libixion
+PROJ_PREFIX=ixion
+PKG_PREFIX=lib$PROJ_PREFIX
 
 # Pick up the version number string from configure.ac.
 VER_MAJOR=$(cat ./configure.ac | grep -E "ixion_major_version.*[0-9]" | sed -e "s/.*\[\([0-9][0-9]*\).*/\1/g")
@@ -21,7 +22,7 @@ echo "| URL | sha256sum | size |"
 echo "|-----|-----------|------|"
 
 for _PKG in $PKGS; do
-    _URL="[$_PKG](https://kohei.us/files/$PKG_PREFIX/src/$_PKG)"
+    _URL="[$_PKG](https://kohei.us/files/$PROJ_PREFIX/src/$_PKG)"
     _HASH=$(sha256sum $_PKG | sed -e "s/^\(.*\)$PKG_PREFIX.*/\1/g" | tr -d "[:space:]")
     _SIZE=$(stat -c "%s" $_PKG)
     echo "| $_URL | $_HASH | $_SIZE |"
