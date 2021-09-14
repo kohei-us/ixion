ee62da6d9025654f26b6cc197a0669cfadf7484a
Kohei Yoshida <kohei.yoshida@gmail.com>
2021-09-13 22:55:23 -0400

    You still need to make the extern "C" function visible...

--- include/ixion/env.hpp
+++ include/ixion/env.hpp
@@ -36,7 +36,7 @@
 #if _WIN32
 #define IXION_MOD_EXPORT __declspec(dllexport)
 #else
-#define IXION_MOD_EXPORT
+#define IXION_MOD_EXPORT __attribute__ ((visibility ("default")))
 #endif
 
 #if defined(IXION_TRACE_ON) || defined(IXION_DEBUG_ON)
