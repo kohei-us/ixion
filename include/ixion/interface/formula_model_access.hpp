c8c64774915883f203388cc8d0d2eb582bdc50d9
Kohei Yoshida <kohei.yoshida@gmail.com>
2021-09-15 22:56:16 -0400

    append_string() doesn't need to be a part of the formula_model_access interface.
    It is supposed to be called only from initial document loading and not
    during formula calculation.

--- include/ixion/interface/formula_model_access.hpp
+++ include/ixion/interface/formula_model_access.hpp
@@ -141,7 +141,6 @@ public:
 
     virtual const table_handler* get_table_handler() const;
 
-    virtual string_id_t append_string(const char* p, size_t n) = 0;
     virtual string_id_t add_string(const char* p, size_t n) = 0;
     virtual const std::string* get_string(string_id_t identifier) const = 0;
 
