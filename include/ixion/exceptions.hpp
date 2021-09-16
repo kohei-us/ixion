7bcdc99800146e4bf0f4dfc2d2a61c3562777be9
Kohei Yoshida <kohei.yoshida@gmail.com>
2021-09-15 20:29:36 -0400

    Update exceptions' signatures to post C++11.

--- include/ixion/exceptions.hpp
+++ include/ixion/exceptions.hpp
@@ -22,8 +22,8 @@ class IXION_DLLPUBLIC general_error : public std::exception
 public:
     general_error();
     explicit general_error(const std::string& msg);
-    virtual ~general_error() throw();
-    virtual const char* what() const throw() override;
+    virtual ~general_error();
+    virtual const char* what() const noexcept override;
 
 protected:
     void set_message(const std::string& msg);
@@ -41,8 +41,8 @@ public:
     explicit formula_error(formula_error_t fe, std::string msg);
     formula_error(formula_error&& other);
 
-    virtual ~formula_error() throw();
-    virtual const char* what() const throw();
+    virtual ~formula_error();
+    virtual const char* what() const noexcept override;
 
     formula_error_t get_error() const;
 };
@@ -51,14 +51,14 @@ class IXION_DLLPUBLIC file_not_found : public general_error
 {
 public:
     explicit file_not_found(const std::string& fpath);
-    virtual ~file_not_found() throw() override;
+    virtual ~file_not_found() override;
 };
 
 class IXION_DLLPUBLIC formula_registration_error : public general_error
 {
 public:
     explicit formula_registration_error(const std::string& msg);
-    virtual ~formula_registration_error() throw() override;
+    virtual ~formula_registration_error() override;
 };
 
 /**
@@ -77,7 +77,7 @@ public:
     };
 
     explicit model_context_error(const std::string& msg, error_type type);
-    virtual ~model_context_error() throw() override;
+    virtual ~model_context_error() override;
 
     error_type get_error_type() const;
 
@@ -89,7 +89,7 @@ class IXION_DLLPUBLIC not_implemented_error : public general_error
 {
 public:
     explicit not_implemented_error(const std::string& msg);
-    virtual ~not_implemented_error() throw() override;
+    virtual ~not_implemented_error() override;
 };
 
 }
--- src/libixion/exceptions.cpp
+++ src/libixion/exceptions.cpp
@@ -16,9 +16,9 @@ general_error::general_error() : m_msg() {}
 general_error::general_error(const std::string& msg) :
     m_msg(msg) {}
 
-general_error::~general_error() throw() {}
+general_error::~general_error() {}
 
-const char* general_error::what() const throw()
+const char* general_error::what() const noexcept
 {
     return m_msg.c_str();
 }
@@ -53,11 +53,11 @@ formula_error::formula_error(formula_error&& other) :
     other.mp_impl = std::make_unique<impl>(formula_error_t::no_error);
 }
 
-formula_error::~formula_error() throw()
+formula_error::~formula_error()
 {
 }
 
-const char* formula_error::what() const throw()
+const char* formula_error::what() const noexcept
 {
     const char* error_name = get_formula_error_name(mp_impl->error);
     if (mp_impl->msg.empty())
@@ -91,12 +91,12 @@ file_not_found::file_not_found(const std::string& fpath) :
     set_message(os.str());
 }
 
-file_not_found::~file_not_found() throw() {}
+file_not_found::~file_not_found() {}
 
 model_context_error::model_context_error(const std::string& msg, error_type type) :
     general_error(msg), m_type(type) {}
 
-model_context_error::~model_context_error() throw() {}
+model_context_error::~model_context_error() {}
 
 model_context_error::error_type model_context_error::get_error_type() const
 {
