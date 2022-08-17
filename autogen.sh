9d28e19f419fb614355143af41af02bfec1f8ed2
Kohei Yoshida <kohei.yoshida@gmail.com>
2022-08-17 16:59:28 -0400

    Update config generation step to use autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,69 +1,16 @@
 #!/bin/sh
 
-if [ -d .git ]; then
-    touch ChangeLog
-    cp README.md README
-fi
-
-TESTLIBTOOLIZE="glibtoolize libtoolize"
-
-LIBTOOLIZEFOUND="0"
-
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 olddir=`pwd`
 cd $srcdir
 
-aclocal --version > /dev/null 2> /dev/null || {
-    echo "error: aclocal not found"
-    exit 1
-}
-automake --version > /dev/null 2> /dev/null || {
-    echo "error: automake not found"
+autoreconf -i || {
+    echo "error: autoreconf failed"
     exit 1
 }
 
-for i in $TESTLIBTOOLIZE; do
-	if which $i > /dev/null 2>&1; then
-		LIBTOOLIZE=$i
-		LIBTOOLIZEFOUND="1"
-		break
-	fi
-done
-
-if [ "$LIBTOOLIZEFOUND" = "0" ]; then
-	echo "$0: need libtoolize tool to build ixion" >&2
-	exit 1
+if test x$NOCONFIGURE = x; then
+    ./configure $@
 fi
-
-amcheck=`automake --version | grep 'automake (GNU automake) 1.5'`
-if test "x$amcheck" = "xautomake (GNU automake) 1.5"; then
-    echo "warning: you appear to be using automake 1.5"
-    echo "         this version has a bug - GNUmakefile.am dependencies are not generated"
-fi
-
-rm -rf autom4te*.cache
-
-$LIBTOOLIZE --force --copy || {
-    echo "error: libtoolize failed"
-    exit 1
-}
-aclocal -I m4 $ACLOCAL_FLAGS || {
-    echo "error: aclocal $ACLOCAL_FLAGS failed"
-    exit 1
-}
-autoheader || {
-    echo "error: autoheader failed"
-    exit 1
-}
-automake -a -c --foreign || {
-    echo "warning: automake failed"
-}
-autoconf || {
-    echo "error: autoconf failed"
-    exit 1
-}
-
-test x$NOCONFIGURE = x && ./configure $@
-
